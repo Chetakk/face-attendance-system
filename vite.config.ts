cd08a0336328183d3fc55aa52b71635c80a3061b
Chetakk <kumarchetak73@gmail.com>
2025-11-07 13:55:19 +0530

    trying to push

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/face-attendance-system/', // <-- project repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
